6a788b30d7f7840f8adbc596715562b7cf72e895
lordjoe2000@gmail.com <lordjoe2000@gmail.com@142b86d5-aa15-ce25-26d2-f8b50552d567>
2013-05-09 10:13:41 +0000

    Changed Proteins to havce ID as well as annotation

---
+++ trunk/jxtandem/src/test/java/org/systemsbiology/xtandem/fdr/FDRUtilitiesTests.java
@@ -0,0 +1,36 @@
+package org.systemsbiology.xtandem.fdr;
+
+import org.junit.*;
+
+/**
+ * org.systemsbiology.xtandem.fdr.FDRUtilitiesTests
+ *
+ * @author Steve Lewis
+ * @date 09/05/13
+ */
+public class FDRUtilitiesTests {
+    public static FDRUtilitiesTests[] EMPTY_ARRAY = {};
+    public static Class THIS_CLASS = FDRUtilitiesTests.class;
+
+    /**
+     * make sure the static asbin function has reasonable limits and enough resolution - > 1 part in 20
+     */
+    @Test
+    public void testAsBin() {
+        // check the limits
+        Assert.assertEquals(0,FDRUtilities.asBin(0));
+        Assert.assertEquals(0,FDRUtilities.asBin(FDRUtilities.MINIMUM_SCORE));
+        Assert.assertEquals(FDRUtilities.NUMBER_BINS - 1,FDRUtilities.asBin(FDRUtilities.MAXIMUM_SCORE));
+        Assert.assertEquals(FDRUtilities.NUMBER_BINS - 1,FDRUtilities.asBin(Double.MAX_VALUE));
+
+        double factor = 1.05;
+        int start = -1;
+        // make sure we have enough distinct values
+        for (double v = FDRUtilities.MINIMUM_SCORE; v < FDRUtilities.MAXIMUM_SCORE; v *= factor) {
+            int test = FDRUtilities.asBin( v); //    get a new value
+            Assert.assertNotSame(test,start);
+            start = test;
+
+        }
+     }
+}
